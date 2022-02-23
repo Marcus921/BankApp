1357b5083f3354b81eecb1e68308652ddc065d56
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2022-02-23 15:15:14 +0100

    Delete BankAppMarcus directory

--- BankAppMarcus/account.py
+++
@@ -1,6 +0,0 @@
-class Account:
-    def __init__(self, accountId, new_accountId, balance, add_balance):
-        self.accountId = accountId
-        self.new_accountId = new_accountId
-        self.balance = balance
-        self.add_balance = add_balance
--- BankAppMarcus/bank.py
+++
@@ -1,6 +0,0 @@
-from account import *
-from customer import *
-
-class Bank(Account, Customer):
-    def __init__(self):
-        self
